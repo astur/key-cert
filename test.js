c3f0d990b951af921209d27fd30134d264f057be
astur <astur@yandex.ru>
2018-06-08 01:18:03 +0300

    first test

--- test.js
+++ test.js
@@ -1,7 +1,14 @@
 const test = require('ava');
 const m = require('.');
+const mockser = require('mockser');
+const got = require('got');
 
-test('key-cert', t => {
-    t.true(true);
-    t.is(m, m);
+test('key-cert', async t => {
+    const server = mockser(await m());
+    server.on('/', (req, res) => {
+        res.end('ok');
+    });
+    await server.listen(1704);
+    await got('https://localhost:1704').then(() => t.pass(), e => t.fail(e));
+    await server.close();
 });
