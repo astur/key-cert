e07ed7ea38ea771d20ed596af79d7c5b062dbbbe
astur <astur@yandex.ru>
2018-06-08 01:18:21 +0300

    first code

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-module.exports = () => {};
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+const pem = require('pem');
+const promisify = require('util').promisify;
+const createCertificate = promisify(pem.createCertificate);
+
+module.exports = async () => {
+    const {serviceKey: key, certificate: cert} = await createCertificate({days: 1, selfSigned: true});
+    return {key, cert};
+};
